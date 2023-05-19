c1e417eaee5c70aca903531823b4ef70ec38726f
tanya-ivanova <ivanova.itanya@gmail.com>
2023-05-19 08:08:22 +0300

    Updating the app

--- starting-project/src/components/NewExpense/ExpenseForm.js
+++ starting-project/src/components/NewExpense/ExpenseForm.js
@@ -4,7 +4,7 @@ import './ExpenseForm.css';
 const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');    
 
     // const[userInput, setUserInput] = useState({
     //     enteredTitle: '',
@@ -47,7 +47,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         }
 
@@ -55,7 +55,7 @@ const ExpenseForm = (props) => {
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
-    };
+    };    
 
     return (
         <form onSubmit={submitHandler} >
@@ -90,6 +90,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
